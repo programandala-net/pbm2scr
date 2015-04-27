4b512369345271dd48daf68d8b4ad07a2461e3f0
Marcos Cruz <github@programandala.net>
2015-04-27 03:05:00 +0200

    Update from development backup 201504270305

--- pbm2scr.fs
+++ pbm2scr.fs
@@ -13,15 +13,29 @@ forth definitions
 
 marker --pbm2scr--
 
-variable width? \ flag: width found in the input file?
-variable p4? \ flag: magic number found in the input file?
+\ \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+\ Requirements
+
+require string.fs  \ Gforth dynamic strings
+
+\ From the Galope library
+\ (http://programandala.net/en.program.galope.html)
+
+: unslurp-file  ( ca1 len1 ca2 len2 -- )
+  \ ca1 len1 = content to write to the file
+  \ ca2 len2 = filename
+  w/o create-file throw >r
+  r@ write-file throw
+  r> close-file throw
+  ;
 
 \ \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 \ The ZX Spectrum screen
 
-\ A ZX Spectrum screen consists of two parts: first, a 256x192 bitmap
-\ that occupies 6144 bytes; second, and a attributes zone of 768 bytes
-\ that describe the color of every 8x8 square of the bitmap.
+\ A ZX Spectrum screen has two parts: first, the bitmap: 6144 bytes
+\ that represent a 256x192 bitmap, with a special order; second, the
+\ attributes: 768 bytes (32x24 character positions) that describe the
+\ colors of every 8x8 square of the bitmap.
 
 6144 constant /zxscr-bitmap
  768 constant /zxscr-attributes
@@ -39,8 +53,8 @@ zxscr-attributes /zxscr-attributes 56 fill  \ white paper, black ink
 
 \ From the documentation of Netpbm (Debian package):
 
-\ ----------------------------------------------
-\ Each PBM image consists of the following:
+\ ---------------------------------------------- Each PBM image
+\ consists of the following:
 \ 
 \ - A "magic number" for identifying the file type.  A pbm image's
 \ magic number is the two characters "P4".
@@ -67,59 +81,129 @@ zxscr-attributes /zxscr-attributes 56 fill  \ white paper, black ink
 \ - Characters  from  a  "#"  to  the  next  end-of-line, before the
 \ width/height line, are comments and are ignored.
 \
+\ There is actually another version of the PBM format, even more more
+\ simplistic, more lavishly wasteful  of space than PBM, called Plain
+\ PBM.
+\
+\ The difference is:
+\
+\ - There is exactly one image in a file.
+\
+\ - The "magic number" is "P1" instead of "P4".
+\
+\ - Each pixel in the raster is represented by a byte containing ASCII
+\ '1' or '0',  representing  black  and white respectively.  There are
+\ no fill bits at the end of a row.
+\
+\ - White space in the raster section is ignored.
+\
+\ - You can put any junk you want after the raster, if it starts with
+\ a white space character.
+\
+\ - No line should be longer than 70 characters.
+\
 \ ----------------------------------------------
 
 \ The chosen approach is to define Forth words, so the PBM file can be
-\ interpreted as a Forth stream file.
+\ interpreted as a Forth source file.
 
 : byte  ( -- b )
-  \ Next byte from the input file.
+  \ Get the next byte from the input file.
   source-id key-file
   ;
-: raster>scr  ( "<bitmap>" -- )
+: scr-address  ( n -- a )
+  \ XXX TODO
+  zxscr +
+  ;
+: p1-bit  ( -- c )
+  \ Get the next bit from the P1 input file,
+  \ represented by a character: "1" or "0".
+  begin   byte dup bl =
+  while   drop  \ skip spaces
+  repeat
+  ;
+: p1-byte  ( -- b )
+  \ Get the next bitmap byte from the P1 input file,
+  \ represented by 8 characters: "1"=black; "0"=white.
+  0  \ return value
+  8 0 do
+    p1-bit [char] 1 = 128 and i rshift or
+  loop
+  ;
+defer bitmap-byte  ( -- b )
+: bitmap>scr  ( "<bitmap>" -- )
+  \ Get the PBM bitmap and convert it to the SCR bitmap.
   /zxscr-bitmap 0 do
-    byte
+    bitmap-byte i scr-address c!
   loop
   ;
 
-wordlist constant pbm-wid
+variable width? \ flag: width found in the input file?
+variable p1? \ flag: P1 magic number found in the input file?
+variable p4? \ flag: P4 magic number found in the input file?
+
+: check-type  ( -- )
+  \ Abort if no file type was specified in the file header.
+  p1? @ p4? @ or 0=
+  abort" File type not supported"
+  ;
+: check-width  ( -- )
+  \ Abort if no width was specified in the file header.
+  width? @ 0= abort" The bitmap size must be 256x192"
+  ;
+
+wordlist constant pbm-wid  \ words allowed in the PBM file
 pbm-wid set-current
 
-: p4  ( -- )
-  \ The first "word" in a binary PBM file is the magic number "P4".
-  ." magic number is ok" cr  \ XXX INFORMER
-  p4? on
-  ;
 ' \ alias #  ( "ccc<newline>" -- )
-  \ Line comments can be included in the image file, with "#".
+: p1  ( -- )
+  \ Set a P1 PBM, the ASCII variant of the format.
+  p1? on  ['] p1-byte is bitmap-byte
+  ;
+: p4  ( -- )
+  \ Set a P4 PBM, the binary variant of the format.
+  p4? on  ['] byte is bitmap-byte
+  ;
 : 256  ( -- )
-  \ The width of the bitmap must be 256.
-  p4? @ 0= abort" File type not supported"
-  width? on  \ mark the width was found
-  ." width is ok" cr  \ XXX INFORMER
+  \ The width of the image.
+  \ This is the last but one metadata before the bitmap.
+  check-type  width? on
   ;
 : 192  ( -- )
-  \ The heigth of the bitmap must be 192.
-  width? @ 0= abort" The bitmap size must be 256x192"
-  ." height is ok" cr  \ XXX INFORMER
-  raster>scr
+  \ The heigth of the image.
+  \ This is the last metadata before the bitmap.
+  \ If everything is ok, get the bitmap.
+  check-type check-width  bitmap>scr
   ;
 
 forth definitions
 
-: (pbm>scr)  ( ca len -- )  \ XXX TMP
-  ." filename=" 2dup type cr  \ XXX INFORMER
-  pbm-wid >order seal
-  included
-  only forth
+: save-scr  ( ca len -- )
+  \ Save the SCR buffer to the output file.
+  \ ca len = input file name
+  s" .scr" s+ zxscr /zxscr 2swap unslurp-file
+  ;
+
+: (pbm>scr)  ( ca len -- )
+  \ ca len = input file name
+  2dup
+  pbm-wid >order seal  included  only forth
+  save-scr
   ;
 : usage  ( -- )
+  cr ." pbm2scr" cr
+  ." Copyright (C) 2015 Marcos Cruz (programandala.net)" cr cr
   ." Usage:" cr
-  ." bpm2scr.fs input.pbm" cr
+  ."   pbm2scr.fs input_file.pbm" cr
+  ." The output file name will have the scr extension added." cr
+  ;
+: parameter?  ( -- f )
+  \ Is there one parameter in the command line?
+  argc @ 2 =
   ;
 : pbm>scr  ( -- )
   \ Convert a PBM file to a ZX Spectrum SCR file.
-  argc @ 2 = if  1 arg (pbm>scr)  else  usage  then  bye
+  parameter? if  1 arg (pbm>scr)  else  usage  then  bye
   ;
 
 pbm>scr
